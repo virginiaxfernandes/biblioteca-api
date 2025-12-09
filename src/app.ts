7a9f260c590f00a71b828e47f1158c3ae03f781f
Virgínia Fernandes <virginiaxfernandes@gmail.com>
2025-12-09 14:51:15 +0000

    att

--- src/app.ts
+++ src/app.ts
@@ -2,33 +2,31 @@ import express from "express";
 import { AppDataSource } from "./data-source";
 import { LivroController } from "./controller/LivroController";
 
-
 const PORT = process.env.PORT || 3000;
 
-
 AppDataSource.initialize()
-.then(() => {
-console.log("Data Source inicializado.");
+  .then(() => {
+    console.log("Data Source inicializado.");
 
+    const app = express();
+    app.use(express.json());
 
-const app = express();
-app.use(express.json());
+    app.get("/", (req, res) => {
+      res.send("API da Biblioteca funcionando! Use /api/livros");
+    });
 
+    const base = "/api/livros";
 
-const base = "/api/livros";
+    app.post(base, LivroController.criar);
+    app.get(base, LivroController.listar);
+    app.get(`${base}/:id`, LivroController.obterPorId);
+    app.put(`${base}/:id`, LivroController.atualizar);
+    app.delete(`${base}/:id`, LivroController.excluir);
 
-
-app.post(base, LivroController.criar);
-app.get(base, LivroController.listar);
-app.get(`${base}/:id`, LivroController.obterPorId);
-app.put(`${base}/:id`, LivroController.atualizar); // pode trocar por PATCH se preferir
-app.delete(`${base}/:id`, LivroController.excluir);
-
-
-app.listen(PORT, () => {
-console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
-})
-.catch((err) => {
-console.error("Erro inicializando Data Source:", err);
-});
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando em http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Erro inicializando Data Source:", err);
+  });
